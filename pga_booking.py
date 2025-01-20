fef94ce227535c95c2346617237eaae5ddbab36d
realcdugz <connorduggan30@gmail.com>
2025-01-20 09:07:00 -0600

    commented out 30min and added headless

--- pga_booking.py
+++ pga_booking.py
@@ -58,13 +58,13 @@ def try_book_bay(driver, bay_name):
     
     try:
         # Select 30-minute lesson
-        wait = WebDriverWait(driver, 10)
-        choose_lesson = wait.until(
-            EC.presence_of_element_located((By.ID, "select_service"))
-        )
-        select = Select(choose_lesson)
-        select.select_by_value("38507")
-        time.sleep(2)
+        # wait = WebDriverWait(driver, 10)
+        # choose_lesson = wait.until(
+        #     EC.presence_of_element_located((By.ID, "select_service"))
+        # )
+        # select = Select(choose_lesson)
+        # select.select_by_value("38507")
+        # time.sleep(2)
 
         wait = WebDriverWait(driver, 10)
         facility_dropdown = wait.until(
@@ -147,13 +147,13 @@ def book_golf_bay():
     
     #Chrome Driver Settings:
     chrome_options = Options()
-    # chrome_options.add_argument('--headless')  # Basic headless mode
-    # chrome_options.add_argument('--disable-gpu')  # Required for headless
-    # chrome_options.add_argument('--no-sandbox')  # Required for headless
-    # chrome_options.add_argument('--disable-dev-shm-usage')  # Required for headless
-    # chrome_options.add_argument('--window-size=1920,1080')  # Set window size
-    # chrome_options.add_argument('--disable-notifications')  # Disable notifications
-    # chrome_options.add_argument('--disable-extensions')  # Disable extensions
+    chrome_options.add_argument('--headless')  # Basic headless mode
+    chrome_options.add_argument('--disable-gpu')  # Required for headless
+    chrome_options.add_argument('--no-sandbox')  # Required for headless
+    chrome_options.add_argument('--disable-dev-shm-usage')  # Required for headless
+    chrome_options.add_argument('--window-size=1920,1080')  # Set window size
+    chrome_options.add_argument('--disable-notifications')  # Disable notifications
+    chrome_options.add_argument('--disable-extensions')  # Disable extensions
     
     try:
         logger.info("Initializing Chrome driver")
